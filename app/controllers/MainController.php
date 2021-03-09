0b1d31878968b7c9329065fd0b22213535099838
Andrey Kozlov <andreykozlov03@yandex.ru>
2021-03-09 21:02:00 +0300

    Урок 14. Добавляет шаблон сайта

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,8 +3,6 @@
 namespace app\controllers;
 
 use ishop\App;
-use ishop\Cache;
-use \RedBeanPHP\R as R;
 
 class MainController extends AppController
 {
@@ -12,20 +10,7 @@ class MainController extends AppController
     public function indexAction()
     {
         $this->setMeta(App::$app->getProperty('name_ishop'), 'Описание...', 'Ключевики');
-        
-        $posts = R::findAll('test');
-        
-        $name = 'Jhon';
-        $age  = 30;
-        
-        
-        $names = ['Andrey', 'Jane', 'Mike'];
-        $cache = new Cache();
-//        $cache->set('test', $names);
-//        $cache->delete('test');
-        
-        
-        $this->set(compact('name', 'age', 'posts'));
+
     }
     
 }
